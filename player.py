20743bf19c47d6839270bbcf48a463fe398d2440
lcicek <lunay.cicek3@gmail.com>
2023-08-02 15:03:25 +0200

    Redid player class and player graphics.

--- player.py
+++ player.py
@@ -1,12 +1,58 @@
-from graphic import Graphic
-from parameters import PLAYER_SPRITE
+from playerAnimation import PlayerAnimation
+from parameters import (
+    LEFT, RIGHT, UP, DOWN, 
+    STANDING, WALKING, SPRINTING
+)
 
 class Player:
     def __init__(self) -> None:
         self.x = 0
         self.y = 0
-        self.graphic = Graphic(PLAYER_SPRITE)
 
-    def updatePos(self, delta_x, delta_y):
+        self.action = STANDING # STANDING, WALKING, SPRINTING
+        self.direction = DOWN # LEFT, RIGHT, UP, DOWN
+        self.move_state = (self.action, self.direction)
+        self.move_bit = 0 # to track left and right step for animation (no matter the direction)
+
+        self.animation = PlayerAnimation()
+
+    def turn(self, delta_x, delta_y):
+        self.update_direction(delta_x= delta_x, delta_y=delta_y)
+
+    def update_position(self, delta_x, delta_y):
         self.x += delta_x
         self.y += delta_y
+
+    def update_move_bit(self):
+        self.move_bit ^= 1 # flip bit
+
+    def update_move_state(self):
+        self.move_state = (self.action, self.direction)
+
+    def update_action(self, action):
+        self.action = action
+        self.update_move_state()
+
+    def update_direction(self, delta_x, delta_y):
+        delta = (delta_x, delta_y)
+
+        if delta == (-1, 0):
+            self.direction = LEFT
+        elif delta == (1, 0):
+            self.direction = RIGHT
+        elif delta == (0, -1):
+            self.direction = UP
+        elif delta == (0, 1):
+            self.direction = DOWN
+        else:
+            raise RuntimeError("The delta_x, delta_y tuple is invalid.")
+
+        self.update_move_bit()
+        self.update_move_state()
+
+    def is_in_bounds(self, location, delta_x, delta_y):
+        """ Returns true if the next move is still in bounds of location."""
+        next_x = self.x + delta_x
+        next_y = self.y + delta_y
+
+        return next_x >= 0 or next_y >= 0 or next_x < location.max_x or next_y < location.max_y
---
+++ playerAnimation.py
@@ -0,0 +1,55 @@
+from graphic import Graphic
+from parameters import (
+    STANDING_FRONT, STANDING_BACK, STANDING_LEFT, STANDING_RIGHT,
+    WALKING_FRONT, WALKING_BACK, WALKING_LEFT, WALKING_RIGHT,
+    WALKING_FRONT_2, WALKING_BACK_2, WALKING_LEFT_2, WALKING_RIGHT_2,
+    LEFT, RIGHT, UP, DOWN,
+    STANDING, WALKING, SPRINTING
+)
+
+class PlayerAnimation:
+    def __init__(self) -> None:
+        self.frames = []
+        self.active_frame = None
+
+        self.initialize_frames()
+        self.initialize_active_frame()
+
+    def initialize_active_frame(self):
+        self.active_frame = self.frames[0][(STANDING, DOWN)].scaled_image
+
+    def initialize_frames(self):
+        standing = {
+            (STANDING, DOWN): Graphic(STANDING_FRONT),
+            (STANDING, UP): Graphic(STANDING_BACK),
+            (STANDING, LEFT): Graphic(STANDING_LEFT),
+            (STANDING, RIGHT): Graphic(STANDING_RIGHT)
+        }
+
+        walking = {
+            (WALKING, DOWN): (Graphic(WALKING_FRONT), Graphic(WALKING_FRONT_2)),
+            (WALKING, UP): (Graphic(WALKING_BACK), Graphic(WALKING_BACK_2)),
+            (WALKING, LEFT): (Graphic(WALKING_LEFT), Graphic(WALKING_LEFT_2)),
+            (WALKING, RIGHT): (Graphic(WALKING_RIGHT), Graphic(WALKING_RIGHT_2))
+        }
+
+        frames = []
+        frames.extend([standing, walking])
+        self.frames = frames
+
+    def set_active_frame(self, move_state, move_bit=None):
+        if move_state in self.frames[0]:
+            self.active_frame = self.get_standing_frame(move_state)
+        elif move_bit is not None and move_state in self.frames[1]:
+            self.active_frame = self.get_walking_frame(move_state=move_state, move_bit=move_bit)
+        else:
+            raise RuntimeError("Found invalid state while trying to find current player frame.")
+
+    def get_standing_frame(self, move_state):
+        return self.frames[0][move_state].scaled_image
+    
+    def get_walking_frame(self, move_state, move_bit):
+        walk_frames = self.frames[1](move_state)
+        frame = walk_frames[move_bit] # left or right step of the two walking frames
+
+        return frame.scaled_image
